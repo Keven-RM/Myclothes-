e6147c9a2d64a34295e1d078e559ee842a2e060c
Keven <keven.rm2004@gmail.com>
2020-10-21 20:19:59 -0300

    Ajuste para receber as informações do backend

--- frontend/src/pages/Produto/index.js
+++ frontend/src/pages/Produto/index.js
@@ -1,49 +1,53 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './styles.css'
-import Image from './image.png';
-
 import {BsArrowLeft} from 'react-icons/bs'
 
-const Produto = () =>{
+import api from '../../services/api'
+
+
+export default class Lista extends Component{
+    state = {
+    product: []
+};
+
+componentDidMount(){
+    this.LoadProducts();
+}
+
+
+LoadProducts = async () =>{
+    const response = await api.get(`produto/${this.props.url}`)
+    this.setState({ product: response.data });
+    console.log(response.data)
+};
+
+    render(){        
+        const { product } = this.state;
     return(
         <>
         <div className="voltar">
             <a href="#"><BsArrowLeft /> Voltar</a>
         </div>
             <article>
+
                 <div className="produto">
                     <div className="produto-imagem">
-                        <img src={Image} alt={Image} />
+            
+                <img src={product.imagem} alt={product.nome} />
                     </div>
                 </div>
+                
                 <div className="produto-info">
-                    <h2 className="titulo">Tênis Nike 270</h2>
-                    <p className="genero">Gênero: Masculino</p>
-                    <p className="marca">Marca: nike</p>
-                    <h2 className="preco">R$ 370,00</h2>
-
+                    <h2 className="titulo">{product.nome}</h2>
+                    <p className="info">Gênero: {product.genero}</p>
+                    <p className="info">Marca: {product.marca}</p>
+                    <p className="info">Detalhes: {product.descricao}</p>
+                    <h2 className="preco">R$ {product.valor}</h2>
                 <div className="cor"> 
                     <form>
                     <h2>Cor</h2>
-                    <label for="preto"><div></div></label>
-                    <input type="radio" name="cor" id="preto" value="preto" />
-
-                    <label for="azul"><div></div></label>
-                    <input type="radio" name="cor" id="azul" value="azul" />
-
-                    <label for="vermelho"><div></div></label>
-                    <input type="radio" name="cor" id="vermelho" value="vermelho" />
-
-                    <label for="amarelo"><div></div></label>
-                    <input type="radio" name="cor" id="amarelo" value="amarelo" />
-
-                    <label for="verde"><div></div></label>
-                    <input type="radio" name="cor" id="verde" value="verde" />
-
-                    <label for="branco"><div></div></label>
-                    <input type="radio" name="cor" id="branco" value="branco" />
-
+                    <label for={product.cor}><div></div></label>
+                    <input type="radio" id={product.cor} />
                     </form>
                 </div>
 
@@ -55,6 +59,5 @@ const Produto = () =>{
             </article>
         </>
     );
+    }
 }
-
-export default Produto
